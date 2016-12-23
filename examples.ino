47467827967c07abad38a0de2aab1184fa454905
dpwicaksa <cureinside@gmail.com>
2016-12-23 11:08:51 +0700

    Add files via upload

---
+++ examples.ino
@@ -0,0 +1,23 @@
+#include <Debug.h>
+
+
+Debug deb(13); //attach LED for debugging at pin 13
+
+void setup() {
+
+}
+
+int i=0;
+void loop() {
+   i+=1;
+   if (i%2==0) 
+    deb.setActive(); else
+      deb.setUnactive();
+   /*
+    * will print message and blinks LED for every even loop,
+    */
+
+   deb.prints ("this is loop - %d",i); //prints
+   deb.LED(); //blink LED attached at process2
+   delay(1000);
+}
